79e5a9c505ddf3af1d1b1ce2b6d4bd21e16c494c
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2017-04-04 22:17:00 +0100

    Display a list of customers and movies. Changed routing

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -11,28 +11,28 @@ namespace Vidly.Controllers
     public class MoviesController : Controller
     {
         // GET: Movies
-        public ActionResult Random()
-        {
-            var movie = new Movie() { Name = "Shrek!" };
-            var customers = new List<Customer>
-            {
-                new Customer {Name = "Customer 1"},
-                new Customer {Name = "Customer 2"}
-            };
+        //public ActionResult Random()
+        //{
+        //    var movie = new Movie() { Name = "Shrek!" };
+        //    var customers = new List<Customer>
+        //    {
+        //        new Customer {Name = "Customer 1"},
+        //        new Customer {Name = "Customer 2"}
+        //    };
 
-            var viewModel = new RandomMovieViewModel
-            {
-                Movie = movie,
-                Customers = customers
-            };
-
-
-            return View(viewModel);
+        //    var viewModel = new RandomMovieViewModel
+        //    {
+        //        Movie = movie,
+        //        Customers = customers
+        //    };
 
 
-        }
+        //    return View(viewModel);
 
-        //  [Route("movies/released/{year}/{month:regex(//d{4}):range(1, 12}")]
+
+        //}
+
+      
 
         public ActionResult ByReleaseYear(int year, int month)
         {
@@ -49,16 +49,21 @@ namespace Vidly.Controllers
             return Content("id= " + id);
         }
 
-        // movies
-        public ActionResult Index(int? pageIndex, string sortBy)
+
+        public ViewResult Index()
         {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
+            var movies = GetMovies();
 
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+            return View(movies);
+        }
 
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie {Id = 1, Name = "Shrek"},
+                new Movie {Id = 2, Name = "Wall-e"}
+            };
         }
     }
 }
